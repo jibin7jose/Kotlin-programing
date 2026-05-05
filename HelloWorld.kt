f4bc348aa1430880d2bdb71cfd052f4193536331
jibin jose <jibinjose884@gmail.com>
2026-05-05 22:02:47 +0530

    Create HelloWorld.kt with a greeting message
    Added a basic Kotlin program that prints a greeting.

---
+++ HelloWorld.kt
@@ -0,0 +1,21 @@
+/*
+-----------------------------------------
+File Name: HelloWorld.kt
+Description: Basic Kotlin program to print a message
+Author: Jibin Jose
+-----------------------------------------
+
+Compile:
+kotlinc HelloWorld.kt -include-runtime -d HelloWorld.jar
+
+Run:
+java -jar HelloWorld.jar
+
+Output:
+Hello, Kotlin!
+-----------------------------------------
+*/
+
+fun main() {
+    println("Hello, Kotlin!")
+}
